cb71f28440830c88025c2784f7eee2b8df482dea
Rafael Correa <rafaelscorreadev@gmail.com>
2025-05-06 16:15:56 -0300

    fix: Resolve conflitos de merge e simplifica configuração de testes

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -94,6 +94,9 @@ DATABASES = {
         'PASSWORD': os.environ.get("SQL_PASSWORD", ""),
         'HOST': os.environ.get("SQL_HOST", ""),
         'PORT': os.environ.get("SQL_PORT", ""),
+        'TEST': {
+            'NAME': 'test_' + os.environ.get("SQL_DATABASE", "test_db.sqlite3"),
+        }
     }
 }
 
